ab9a4eae5c7f926ba3662706cf96dec6e018c0c7
Gonzalo Santa Cruz <gonzalo.invernizzi@gmail.com>
2014-10-23 12:31:03 -0300

    vista agregar bienes

--- app/controllers/recepciones_de_bien_de_consumo_controller.rb
+++ app/controllers/recepciones_de_bien_de_consumo_controller.rb
@@ -28,28 +28,29 @@ class RecepcionesDeBienDeConsumoController < ApplicationController
   def create
 
    
-    #@rbc = RecepcionDeBienDeConsumo.create!(fecha:DateTime.now, estado: RecepcionDeBienDeConsumo::ESTADOS[(params[:estado_id])]) 
-    @rbc = RecepcionDeBienDeConsumo.create(fecha:DateTime.now, estado: params[:e][:estado_id], descripcion_provisoria: params[:recepcion_de_bien_de_consumo][:descripcion_provisoria])  
+    #@recepcion_de_bien_de_consumo = RecepcionDeBienDeConsumo.create!(fecha:DateTime.now, estado: RecepcionDeBienDeConsumo::ESTADOS[(params[:estado_id])]) 
+    @recepcion_de_bien_de_consumo = RecepcionDeBienDeConsumo.create(fecha:DateTime.now, estado: params[:e][:estado_id], descripcion_provisoria: params[:recepcion_de_bien_de_consumo][:descripcion_provisoria])  
     @tddp = TipoDeDocumento.find(params[:tdp][:tipo_de_documento_id])
     @tdds = TipoDeDocumento.find(params[:tds][:tipo_de_documento_secundario_id])
 
     @docRecepcion_p = DocumentoDeRecepcion.create!(numero_de_documento: params[:numero_doc_principal], tipo_de_documento: @tddp)
     @docRecepcion_s = DocumentoDeRecepcion.create!(numero_de_documento: params[:numero_doc_secundario], tipo_de_documento: @tdds)        
 
-    @rbc.create_documento_principal(documento_de_recepcion:@docRecepcion_p, 
-                                    recepcion_de_bien_de_consumo: @rbc)
+    @recepcion_de_bien_de_consumo.create_documento_principal(documento_de_recepcion:@docRecepcion_p, 
+                                    recepcion_de_bien_de_consumo: @recepcion_de_bien_de_consumo)
 
-    @rbc.documentos_secundario.create!(documento_de_recepcion:@docRecepcion_s, 
-                                       recepcion_de_bien_de_consumo: @rbc)    
+    @recepcion_de_bien_de_consumo.documentos_secundario.create!(documento_de_recepcion:@docRecepcion_s, 
+                                       recepcion_de_bien_de_consumo: @recepcion_de_bien_de_consumo)    
 
 
     respond_to do |format|
-      if @rbc.save
-        format.html { redirect_to @rbc, notice: 'Recepcion de bien de consumo was successfully created.' }
-        format.json { render :show, status: :created, location: @rbc }
+      if @recepcion_de_bien_de_consumo.save
+        #format.html { redirect_to @recepcion_de_bien_de_consumo, notice: 'Recepcion de bien de consumo fue creado exitosamente.' }
+        format.html { render :new_bienes, notice: 'Recepcion de bien de consumo fue creado exitosamente.' }
+        format.json { render :show, status: :created, location: @recepcion_de_bien_de_consumo }
       else
         format.html { render :new }
-        format.json { render json: @rbc.errors, status: :unprocessable_entity }
+        format.json { render json: @recepcion_de_bien_de_consumo.errors, status: :unprocessable_entity }
       end
     end
   end
