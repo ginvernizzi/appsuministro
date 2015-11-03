ac38239f16bf993256a561259d1a1ba85e05a0d1
ginvernizzi <gonzalo.invernizzi@gmail.com>
2015-11-03 16:04:56 -0300

    bugs ricardo 23/10

--- app/assets/javascripts/bienes_de_consumo/bienes_de_consumo.js
+++ app/assets/javascripts/bienes_de_consumo/bienes_de_consumo.js
@@ -2,38 +2,38 @@
 var ready = function() {
 
   //************ Muestra una lista al colocar los 4 digitos del codigo  **********//
-  //$("#clase_codigo").keypress(function(){   
-  $("#bien_de_consumo_codigo").on("keyup", function(e) {
-      var str = this.value;
-      var n = str.length;      
+  // //$("#clase_codigo").keypress(function(){   
+  // $("#bien_de_consumo_codigo").on("keyup", function(e) {
+  //     var str = this.value;
+  //     var n = str.length;      
       
-      if(n == 4)
-      { buscar_codigos_existentes(str); }
-      else
-      { 
-        $('#titulo').html("");
-        $('#tabla_items_existentes').html(""); 
-      }         
-  });   
+  //     if(n == 4)
+  //     { buscar_codigos_existentes(str); }
+  //     else
+  //     { 
+  //       $('#titulo').html("");
+  //       $('#tabla_items_existentes').html(""); 
+  //     }         
+  // });   
 
-  function buscar_codigos_existentes(codigo)
-  {   
-    $.ajax({
-      type: "get",
-      dataType: "json",
-      url: "/bienes_de_consumo/traer_clases_con_codigo_de_bien_existente",      
-      data: { codigo: codigo },        
-      success: function(data){            
-            //BlanquearCampos();
-            $('#titulo').html("<b> Clases con codigo de Bien de consumo existente </b>");
-            $('#tabla_items_existentes').html(data);
-      },
-      error: function (request, status, error) 
-          {             
-            alert("Ha ocurrido un error");
-          }
-    });       
-  }
+  // function buscar_codigos_existentes(codigo)
+  // {   
+  //   $.ajax({
+  //     type: "get",
+  //     dataType: "json",
+  //     url: "/bienes_de_consumo/traer_clases_con_codigo_de_bien_existente",      
+  //     data: { codigo: codigo },        
+  //     success: function(data){            
+  //           //BlanquearCampos();
+  //           $('#titulo').html("<b> Clases con codigo de Bien de consumo existente </b>");
+  //           $('#tabla_items_existentes').html(data);
+  //     },
+  //     error: function (request, status, error) 
+  //         {             
+  //           alert("Ha ocurrido un error");
+  //         }
+  //   });       
+  // }
   //********************************************************************//
 
   $('#bien_de_consumo_clase_id').change(function() {
