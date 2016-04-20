d3fd219a5e4a436ceb277bd30237c04661b65482
ginvernizzi <gonzalo.invernizzi@gmail.com>
2016-04-20 16:36:27 -0300

    traer consumos por partida parcial, falta impresion

--- app/assets/javascripts/consumos_directo/consumos_directo.js
+++ app/assets/javascripts/consumos_directo/consumos_directo.js
@@ -3,6 +3,7 @@ $(document).on("ready page:load", function() {
   //mascara entero, 6
   $('#cantidad_a_consumir').inputmask('999999', { clearMaskOnLostFocus: true, placeholder: ' ' })    
   $("#codigo").inputmask("9.9.9.99999.9999", { clearMaskOnLostFocus: true })   
+  $("#partida_parcial").inputmask("999", { clearMaskOnLostFocus: true })
   
   var currentDate = new Date();
   $('#consumo_directo_fecha').datepicker
