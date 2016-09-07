c428203e81dec54649d52e6f62c11324d519f608
ginvernizzi <gonzalo.invernizzi@gmail.com>
2016-09-07 14:49:20 -0300

    se modificaron los archivos de style

--- app/assets/javascripts/clases/clases.js
+++ app/assets/javascripts/clases/clases.js
@@ -1,68 +1,68 @@
 $(document).on("ready page:load", function() {
- 
-  //$('#clase_codigo').inputmask('99999')      
-  //$("#clase_codigo").keypress(function(){   
+
+  //$('#clase_codigo').inputmask('99999')
+  //$("#clase_codigo").keypress(function(){
   $("#clase_codigo").on("keyup", function(e) {
       var str = this.value;
-      var n = str.length;      
-      
+      var n = str.length;
+
       if(n == 5)
       { buscar_codigos_existentes(str); }
       else
-      { 
-        $('#titulo').html("")          
-        $('#tabla_items_existentes').html("") 
-      }         
-  }); 
+      {
+        $('#titulo').html("")
+        $('#tabla_items_existentes').html("")
+      }
+  });
 
   function buscar_codigos_existentes(codigo)
-  {   
+  {
     $.ajax({
       type: "get",
       dataType: "json",
-      url: "/clases/traer_partidas_parciales_con_codigo_de_clase_existente",      
-      data: { codigo: codigo },        
-      success: function(data){            
+      url: "/clases/traer_partidas_parciales_con_codigo_de_clase_existente",
+      data: { codigo: codigo },
+      success: function(data){
             //BlanquearCampos();
             $('#titulo').html("<b> Partidas Parciales con codigo de clase existente  </b>");
             $('#tabla_items_existentes').html(data);
       },
-      error: function (request, status, error) 
-          {             
+      error: function (request, status, error)
+          {
             alert("Ha ocurrido un error");
           }
-    });       
+    });
   }
-  
-  $("#buscar_nombres_repetidos").on('click', function() {                        
-      buscar_nombres_existentes($("#clase_nombre").val());      
-      //$('#tabla_items_existentes').html("") 
+
+  $("#buscar_nombres_repetidos").on('click', function() {
+      buscar_nombres_existentes($("#clase_nombre").val());
+      //$('#tabla_items_existentes').html("")
   })
 
   function buscar_nombres_existentes(nombre)
-  {       
+  {
     $.ajax({
       type: "get",
       dataType: "json",
-      url: "/clases/traer_partidas_parciales_con_nombre_de_clase_similar",      
-      data: { nombre: nombre },        
-      success: function(data){                
+      url: "/clases/traer_partidas_parciales_con_nombre_de_clase_similar",
+      data: { nombre: nombre },
+      success: function(data){
             $('#titulo').html("<b> Partidas Parciales con nombre de clase similares <b>");
             $('#tabla_items_existentes').html(data);
       },
-      error: function (request, status, error) 
-          {                       
+      error: function (request, status, error)
+          {
             alert("Ha ocurrido un error");
           }
-    });       
+    });
   }
 
-  $('#clase_nombre').bind('railsAutocomplete.select', function(event, data){          
-    $("#categoria_clase_id").val(data.item.id);    
+  $('#clase_nombre').bind('railsAutocomplete.select', function(event, data){
+    $("#categoria_clase_id").val(data.item.id);
     $("#categoria_clase_id").change();
   });
 
-  $('#clase_nombre_all_clases').on('railsAutocomplete.select', function(event, data){ 
+  $('#clase_nombre_all_clases').on('railsAutocomplete.select', function(event, data){
     $("#clase_id").val(data.item.id);
   });
 
@@ -78,22 +78,22 @@ $(document).on("ready page:load", function() {
     $.ajax({
     type: "get",
     dataType: "json",
-    url: "/clases/traer_clase_por_id",            
-    data: { clase_id: clase_id }, 
-    success: function(data){            
+    url: "/clases/traer_clase_por_id",
+    data: { clase_id: clase_id },
+    success: function(data){
       $('#clase_id').val("");
       $('#clase_nombre_all_clases').val("");
       $('#tabla_todas_las_clases').html(data)
     },
-    error: function (request, status, error) 
-        { 
+    error: function (request, status, error)
+        {
           alert("Ha ocurrido un error. No se mostrarán las clases");
         }
-    });       
+    });
   }
 
   $("#traer_todas_las_clases").on('click', function(){
-      {traer_todas_las_clases();} 
+      {traer_todas_las_clases();}
   });
 
   function traer_todas_las_clases()
@@ -101,18 +101,18 @@ $(document).on("ready page:load", function() {
     $.ajax({
     type: "get",
     dataType: "json",
-    url: "/clases/traer_todas_las_clases",            
-    data: {}, 
-    success: function(data){            
+    url: "/clases/traer_todas_las_clases",
+    data: {},
+    success: function(data){
       $('#clase_id').val("");
       $('#clase_nombre_all_clases').val("");
       $('#tabla_todas_las_clases').html(data)
     },
-    error: function (request, status, error) 
-        { 
+    error: function (request, status, error)
+        {
           alert("Ha ocurrido un error. No se mostrarán las clases");
         }
-    });       
+    });
   }
 
 });
