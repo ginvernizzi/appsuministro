56ca499ad96057e4cc171e23140bc3f83305f266
ginvernizzi <gonzalo.invernizzi@gmail.com>
2016-10-19 13:13:07 -0300

    columna costo mal, en reporte de ver_consumos_por_obra_proyecto_y_fecha1

--- app/models/generador_de_impresion_items_de_consumo.rb
+++ app/models/generador_de_impresion_items_de_consumo.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 class GeneradorDeImpresionItemsDeConsumo
-	include ApplicationHelper 
+	include ApplicationHelper
 	include ActionView::Helpers::NumberHelper
 
 	def initialize
@@ -8,7 +8,7 @@ class GeneradorDeImpresionItemsDeConsumo
 	end
 
 	def generar_pdf_items_consumo_directo(bienes_de_consumo_para_consumir)
-		bienes = bienes_de_consumo_para_consumir		
+		bienes = bienes_de_consumo_para_consumir
 
 		@ruta_plantilla = Rails.root.join("app/plantillas/formulario_comprobante_consumos_por_obra_proyecto.odt")
 
@@ -16,25 +16,25 @@ class GeneradorDeImpresionItemsDeConsumo
 			r.add_field("FECHA", I18n.l(DateTime.now))
 			r.add_field("OBRA_PROYECTO", bienes[0].consumo_directo.obra_proyecto.descripcion)
 			r.add_field("COSTO_TOTAL_GENERAL", obtener_total_general(bienes))
-								
-			r.add_table("TABLA_CONSUMO_DIRECTO", bienes) do |s|								
+
+			r.add_table("TABLA_CONSUMO_DIRECTO", bienes) do |s|
 				s.add_column("FECHA_CONSUMO") { |i| i.consumo_directo.fecha.strftime("%d/%m/%Y") }
-				s.add_column("COMPROBANTE") { |i| i.consumo_directo.id }							
-				s.add_column("CLASE") { |i| i.bien_de_consumo.clase.nombre }							
-				s.add_column("CODIGO") { |i| obtener_codigo_completo_bien_de_consumo(i.bien_de_consumo.nombre)  }							
-				s.add_column("NOMBRE") { |i| i.bien_de_consumo.nombre }				
-				s.add_column("AREA_DESTINO") { |i| i.consumo_directo.area.nombre }							
+				s.add_column("COMPROBANTE") { |i| i.consumo_directo.id }
+				s.add_column("CLASE") { |i| i.bien_de_consumo.clase.nombre }
+				s.add_column("CODIGO") { |i| obtener_codigo_completo_bien_de_consumo(i.bien_de_consumo.nombre)  }
+				s.add_column("NOMBRE") { |i| i.bien_de_consumo.nombre }
+				s.add_column("AREA_DESTINO") { |i| i.consumo_directo.area.nombre }
 				s.add_column("CANTIDAD") { |i| i.cantidad }
-				s.add_column("COSTO") { |i| number_to_currency(CostoDeBienDeConsumo.where("bien_de_consumo_id = ?", i.bien_de_consumo_id).last.costo, precision: 3) }
-				s.add_column("COSTO_TOTAL") { |i| number_to_currency(obtener_costo_total(CostoDeBienDeConsumo.where("bien_de_consumo_id = ?", i.bien_de_consumo_id).last.costo, i.cantidad), precision: 3) }
-				s.add_column("DESCRIPCION") { |i| i.descripcion_de_recepcion }					 			
+				s.add_column("COSTO") { |i| number_to_currency(i.costo, precision: 3) }
+				s.add_column("COSTO_TOTAL") { |i| number_to_currency(obtener_costo_total(i.costo, i.cantidad), precision: 3) }
+				s.add_column("DESCRIPCION") { |i| i.descripcion_de_recepcion }
 			end
 		end
 
 		@ruta_formulario_interno_odt = Rails.root.join("public/forms_impresiones/" + nombre_formulario_consumo_items_odt)
 		report.generate(@ruta_formulario_interno_odt)
 		@ruta_formularios_internos = Rails.root.join("public/forms_impresiones/")
-		`unoconv -f pdf #{@ruta_formulario_interno_odt}`						
+		`unoconv -f pdf #{@ruta_formulario_interno_odt}`
 	end
 
 	def nombre_formulario_consumo_items_pdf
@@ -42,7 +42,7 @@ class GeneradorDeImpresionItemsDeConsumo
 	end
 
 	private
-	
+
 	def nombre_formulario_consumo_items_odt
 		nombre_formulario_consumo_items + ".odt"
 	end
