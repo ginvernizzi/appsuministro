9ec11f78a45bf361b474d5a306dfbd053346b08a
ginvernizzi <gonzalo.invernizzi@gmail.com>
2016-12-04 16:25:55 -0300

    ver subtotales en consumos por obra proyecto

--- app/controllers/consumos_directo_controller.rb
+++ app/controllers/consumos_directo_controller.rb
@@ -654,9 +654,35 @@ class ConsumosDirectoController < ApplicationController
     @bien_de_consumo_para_consumir = nil
 
     if !obra_proyecto_id.nil? && !obra_proyecto_id.blank? && !fecha_inicio.nil? && !fecha_fin.nil?
-        #@bien_de_consumo_para_consumir = BienDeConsumoParaConsumir.joins(:bien_de_consumo, :consumo_directo).where("consumos_directo.estado = ? AND consumos_directo.obra_proyecto_id = ? AND consumos_directo.fecha >= ? AND consumos_directo.fecha <= ?", estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin)
-        @bien_de_consumo_para_consumir = BienDeConsumoParaConsumir.bien_de_consumo_para_consumir(estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin)
-        if @bien_de_consumo_para_consumir.count > 0
+        #@bien_de_consumo_para_consumir = BienDeConsumoParaConsumir.bien_de_consumo_para_consumir(estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin).select("bienes_de_consumo_para_consumir.*, areas.nombre, areas.id").order("areas.nombre, areas.id")
+
+        @bien_de_consumo_para_consumir = BienDeConsumoParaConsumir.joins(:consumo_directo => [:area]).where("consumos_directo.estado = ? AND consumos_directo.obra_proyecto_id = ? AND consumos_directo.fecha >= ? AND consumos_directo.fecha <= ?", estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin).select("bienes_de_consumo_para_consumir.*").order("areas.nombre").group("areas.nombre, bienes_de_consumo_para_consumir.id")
+
+        #@subtotales = BienDeConsumoParaConsumir.bien_de_consumo_para_consumir(estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin).select("bienes_de_consumo_para_consumir.*, areas.nombre").group("areas.id, bienes_de_consumo_para_consumir.id").order("areas.id")
+        @subtotales =  BienDeConsumoParaConsumir.joins(:consumo_directo => [:area]).where("consumos_directo.estado = ? AND consumos_directo.obra_proyecto_id = ? AND consumos_directo.fecha >= ? AND consumos_directo.fecha <= ?", estado_activo,obra_proyecto_id,fecha_inicio, fecha_fin).select("consumos_directo.area_id").group("consumos_directo.area_id").sum("cantidad * costo")
+
+        # @connection = ActiveRecord::Base.establish_connection(
+        #     :adapter => "mysql2",
+        #     :host => "localhost",
+        #     :database => "appsuministro_development",
+        #     :username => "suministro",
+        #     :password => "123456"
+        # )
+        #
+        # @subtotales = "SELECT bienes_de_consumo_para_consumir.*, areas.nombre, areas.id as area_id, (costo * cantidad) AS total
+        #         FROM bienes_de_consumo_para_consumir INNER JOIN consumos_directo ON consumos_directo.id = bienes_de_consumo_para_consumir.consumo_directo_id
+        #         INNER JOIN areas ON areas.id = consumos_directo.area_id
+        #         WHERE (consumos_directo.estado = 1 AND consumos_directo.obra_proyecto_id = 1 AND consumos_directo.fecha >= '21-4-2016' AND consumos_directo.fecha <= '1-11-2016')
+        #         GROUP BY areas.id, bienes_de_consumo_para_consumir.id  ORDER BY area_id"
+        #
+        # @result = @connection.connection.execute(@subtotales);
+        # puts "******************************"
+        # puts @result
+
+        @bien_para_consumir_obj = BienDeConsumoParaConsumir.new
+        @lista_final = @bien_para_consumir_obj.lista_final_con_subtotales(@bien_de_consumo_para_consumir, @subtotales)
+
+        if @bien_de_consumo_para_consumir.length > 0
            @bien_de_consumo_para_consumir[0].fecha_inicio_impresion = fecha_inicio;
            @bien_de_consumo_para_consumir[0].fecha_fin_impresion = fecha_fin;
            @bien_de_consumo_para_consumir[0].obra_proyecto_impresion = obra_proyecto_id;
--- app/models/bien_de_consumo_para_consumir.rb
+++ app/models/bien_de_consumo_para_consumir.rb
@@ -11,11 +11,41 @@ class BienDeConsumoParaConsumir < ActiveRecord::Base
   attr_accessor :clase_impresion
   attr_accessor :descripcion_de_recepcion
   attr_accessor :bien_id_impresion
+  attr_accessor :subtotal
 
   #scope :name, joins(:table).where('.field = ?', 'value')
-  scope :bien_de_consumo_para_consumir, ->(estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin) { joins(:consumo_directo).where("consumos_directo.estado = ? AND consumos_directo.obra_proyecto_id = ? AND consumos_directo.fecha >= ? AND consumos_directo.fecha <= ?", estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin) }
-#BienDeConsumoParaConsumir.joins(:consumo_directo).where("consumos_directo.estado = ? AND consumos_directo.obra_proyecto_id = ? AND consumos_directo.fecha >= ? AND consumos_directo.fecha <= ?", estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin)
+  scope :bien_de_consumo_para_consumir, ->(estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin) { joins(:consumo_directo => [:area]).
+  where("consumos_directo.estado = ? AND consumos_directo.obra_proyecto_id = ? AND consumos_directo.fecha >= ? AND consumos_directo.fecha <= ?", estado_activo, obra_proyecto_id, fecha_inicio, fecha_fin) }
 
+  def lista_final_con_subtotales(lista_de_consumos_por_obra_proyecto, subtotales_x_area_destino)
+
+      puts "************** subto"
+      puts subtotales_x_area_destino.length
+      puts "subtol **********************"
+
+    @lista_final = Array.new
+    area_id_actual = lista_de_consumos_por_obra_proyecto.first.consumo_directo.area.id
+    lista_de_consumos_por_obra_proyecto.each do |bien|
+          if bien.consumo_directo.area.id == area_id_actual
+              @lista_final << bien
+              if bien.id == lista_de_consumos_por_obra_proyecto.last.id
+                subtotal_del_area = subtotales_x_area_destino[bien.consumo_directo.area.id]
+                @lista_final.last.update(subtotal: subtotal_del_area)
+              end
+          else
+              subtotal_del_area = subtotales_x_area_destino[area_id_actual]
+              @lista_final.last.update(subtotal: subtotal_del_area)
+              area_id_actual = bien.consumo_directo.area.id
+          end
+    end
+    return @lista_final
+  end
+
+
+
+  def costo_total
+      self.cantidad * costo
+  end
 
 #  scope :created_before, ->(time) { where("created_at < ?", time) }
 end
