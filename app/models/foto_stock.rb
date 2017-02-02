6340a8ea6c8770207a1f8e1939312b25c25d4349
ginvernizzi <gonzalo.invernizzi@gmail.com>
2017-02-02 15:42:48 -0300

    funcion nueva en FotoAStock, para buscar el historial de un bien en el stock

--- app/models/foto_stock.rb
+++ app/models/foto_stock.rb
@@ -1,7 +1,7 @@
 class FotoStock
-	def guardar_stock_a_fecha    
+	def guardar_stock_a_fecha
 		@items_stock_a_fecha = Array.new
-		deposito = Deposito.find(1) #deposito suministro "piso -1" 
+		deposito = Deposito.find(1) #deposito suministro "piso -1"
 		@items_stock = ItemStock.where("deposito_id = ?", deposito.id)
 
 		@items_stock.each do |item_stock|
@@ -11,9 +11,36 @@ class FotoStock
 		end
 		@reporte_a_fecha = ReporteAFecha.new(fecha: DateTime.now, stock_diario: @items_stock_a_fecha.to_json)
 		if @reporte_a_fecha.save
-			Rails.logger.info("El reporte se guardó exitosamente #{Time.zone.now}")        
-		else  
+			Rails.logger.info("El reporte se guardó exitosamente #{Time.zone.now}")
+		else
 			Rails.logger.info("El reporte no pudo ser guardado #{Time.zone.now}")
 		end
 	end
+
+	def test
+	  puts 'hola!!!!'
+	end
+
+	def traer_lista_de_stocks
+	  #@reportes = Array.new
+		@items_stock = Array.new
+	  ReporteAFecha.all.each do |reporte|
+	    @items_de_stock_json = JSON.parse(reporte.stock_diario)
+	    @items_de_stock_json.each do |item|
+				# puts "************* #{item['bien_de_consumo_id'].to_i} *************"
+				if item['bien_de_consumo_id'].to_i == 1715
+		      item_stock_a_fecha = ItemStockAFecha.new(bien_de_consumo_id: item['bien_de_consumo_id'], deposito: Deposito.find(item['deposito_id']), costo: item['costo'], cantidad: item['cantidad'])
+		      @items_stock << item_stock_a_fecha
+					puts "ENTRO UNA VEZ"
+					break
+				else
+					#puts "No entro!!!!!"
+				end
+	    end
+			break
+	  end
+		return	@items_stock
+	end
+
+
 end
